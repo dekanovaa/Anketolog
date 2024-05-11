6d009e0e9074ac43d1f2922cb0d11843ed2398b7
dekanovaa <dekanovasevinchhh@gmail.com>
2024-05-11 21:08:59 +0500

    anketolog_sayti

---
+++ js/main.js
@@ -0,0 +1,7 @@
+let body__button = document.getElementById("#body__button");
+let body__btn = document.getElementById("#body__btn");
+
+body__button.addEventListener("click", function(){
+    body__button.style.display= "none";
+    body__btn.style.display= "block";
+})
